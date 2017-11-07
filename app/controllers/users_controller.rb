44f9f2fb1c9f9dc5fa1fd20a7e97829d0bd7f2af
matumaru <matumarukouhei@gmail.com>
2017-11-07 14:37:36 +0900

    編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(users_params)
+    end
+    return redirect_to :root
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
