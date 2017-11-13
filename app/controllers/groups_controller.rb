e1972c0c491c8fa29939a606a25abd4cf1168e16
matumaru <matumarukouhei@gmail.com>
2017-11-13 20:14:22 +0900

    /group配下修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,39 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit]
+
+  def index
+  end
+
+
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成できました。"
+    else
+      render :new
+      #直接viewファイルへ飛ぶ
+    end
+  end
 
   def edit
   end
 
-  def new
+  def update
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
