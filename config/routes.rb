2de2f367978ad27c517a54a5dbe8eaca679c1329
matumaru <matumarukouhei@gmail.com>
2017-11-14 15:26:43 +0900

    ルーティング　ネスト化

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :groups, only: [:new, :create] do
+    resources :messages, only: [:index]
+  end
   resources :users, only: [:edit, :update]
-  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'messages#index'
 end
