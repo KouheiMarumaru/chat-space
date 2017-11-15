6363abce5348dfd081abf0431a10a706ca6952ae
matumaru <matumarukouhei@gmail.com>
2017-11-15 15:19:46 +0900

    分岐追加、不要な行を削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,9 +23,13 @@ class GroupsController < ApplicationController
 
   def update
     @group.update(group_params)
-    redirect_to root_path, notice: "グループを編集できました。"
+    if @group.save
+      redirect_to root_path, notice: "グループを編集できました。"
+    else
+      render :new
+      #直接viewファイルへ飛ぶ
+    end
   end
-
 
   private
 
