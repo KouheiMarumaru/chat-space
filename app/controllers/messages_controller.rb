2bcecc3f91db074d0f7b11768cd08285fbfc7f97
matumaru <matumarukouhei@gmail.com>
2017-11-18 16:59:00 +0900

    コントローラ改修

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,32 @@
 class MessagesController < ApplicationController
+  before_action :message_params, only: [:edit ,:update, :create, :show]
 
   def index
+    @group = Group.find(params[:group_id])
+    @message = Message.new
+    @messages = @group.messages
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def new
+    @message = Message.find(params[:id])
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  def update
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :img).merge(user_id: current_user.id,
+      group_id: params[:group_id])
   end
 
 end
