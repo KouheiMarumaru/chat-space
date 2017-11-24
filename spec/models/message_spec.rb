71e4610bbb10ac68b7ee53d9e57cf9fae8289a59
matumaru <matumarukouhei@gmail.com>
2017-11-24 19:36:20 +0900

    letの記述追加

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
+    let(:message) { build(:message) }
 
     context 'メッセージを保存できる場合' do
 
       it "メッセージがあれば保存できる" do
-        message = build(:message,img:nil)
+        message.img = nil
         expect(message.valid?).to be_truthy
       end
 
       it "画像があれば保存できる" do
-        message = build(:message,content:nil)
+        message.content = nil
         expect(message.valid?).to be_truthy
       end
 
       it "メッセージと画像があれば保存できる" do
-        message = build(:message)
         expect(message.valid?).to be_truthy
       end
     end
@@ -23,17 +23,18 @@ describe Message do
     context 'メッセージを保存できない場合' do
 
       it "メッセージも画像も無いと保存できない" do
-        message = build(:message,img:nil,content:nil)
+        message.content = nil
+        message.img = nil
         expect(message.valid?).to be_falsey
       end
 
       it "group_idが無いと保存できない" do
-        message = build(:message,group_id:nil)
+        message.group_id = nil
         expect(message.valid?).to be_falsey
       end
 
       it "user_idが無いと保存できない" do
-        message = build(:message,user_id:nil)
+        message.user_id = nil
         expect(message.valid?).to be_falsey
       end
     end
