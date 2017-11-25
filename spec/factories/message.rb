fa815cab9fb63510f92c0bdceaee8b559cae7819
matumaru <matumarukouhei@gmail.com>
2017-11-25 17:07:27 +0900

    コメントアウト削除

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
 
   factory :message do
     content       Faker::Lorem.paragraph
-    # img           Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/img/ltgm.jpg'))
     img   Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/img/lgtm.jpg'))
     user
     group
