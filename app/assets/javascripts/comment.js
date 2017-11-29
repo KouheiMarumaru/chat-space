0077ff6c6d0e8885817619d7ead9028fe55c1857
matumaru <matumarukouhei@gmail.com>
2017-11-29 18:23:03 +0900

    js　ajax用記述追記

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,7 +1,52 @@
 $(function(){
+
+  function buildHTML(message){
+    var html = `<div class="main__post">
+                  <div class="main__post__name">
+                    ${message.name}
+                  </div>
+                  <div class="main__post__time">
+                    ${message.created_at}
+                  </div>
+                  <div class="main__post__text">
+                    ${message.content}
+                  </div>
+                </div>`
+    return html;
+  }
+
   $('#new_comment').on('submit', function(e){
+
+    //デフォルトのブラウザ挙動を抑制
     e.preventDefault();
+
+    //検証
     console.log(this)
+
+    //フォーム情報を取得
     var formData = new FormData(this);
+
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    //jbuilderから送られたデータを受け止める
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $('.main').append(html)
+      $('.textbox').val('')
+    })
+
+    .fail(function(){
+      alert('error');
+    })
+
   })
 })
