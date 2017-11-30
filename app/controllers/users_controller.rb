6aee370d91c10b44004b94a30b6822f4ee0eea7c
matumaru <matumarukouhei@gmail.com>
2017-11-30 15:14:34 +0900

    コントローラ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update,]
 
+  def index
+   respond_to do |format|
+     format.html
+     format.json
+   end
+  end
+
   def edit
   end
 
