f07abe1f94f00b568b9903e0bcd310e4ff114e68
matumaru <matumarukouhei@gmail.com>
2017-11-30 15:14:21 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :new, :create]
   end
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'groups#index'
 end
